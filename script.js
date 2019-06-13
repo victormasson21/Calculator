916ceeff0576cd54939a48205fac7acf2ef75be7
Victor Masson <victormasson21@gmail.com>
2019-06-13 18:49:33 +0200

    Prevents running invalid operations
    Behaves so that the operation:
    - Can't start with '00...'
    - Can't start with an operator
    - Can't end with an operator (gets popped from the operation array)
    - Can't have two operators in a row (newer one replaces older one)
    - Can start with '-' to use negative numbers
    - Can have operators followed by '-' tu use negaitve numbers

--- script.js
+++ script.js
@@ -19,13 +19,21 @@ let pad = {
 
 let operation = [];
 let current = [];
+let operators = ['+', '-', '/', '*']
+
+// getElementById('id')
 
 
 document.addEventListener('click', function(e) {
   for (let key of Object.keys(pad)) {
     if (document.getElementById(key).contains(e.target)) {
+      let lastKey = operation[operation.length-1]
       switch (key) {
         case 'zero':
+          if (operation.includes(0) && operation.length==1) {
+            console.log('one zero is enough');
+            break
+          }
         case 'one':
         case 'two':
         case 'three':
@@ -41,6 +49,7 @@ document.addEventListener('click', function(e) {
 
         case 'decimal':
           if (current.includes('.')) {
+            console.log('double decimal isn\'t a thing');
             break
           } else {
             operation.push(pad[key])
@@ -52,6 +61,27 @@ document.addEventListener('click', function(e) {
         case 'substract':
         case 'multiply':
         case 'add':
+          if (current.length == 0) {
+            if (key == 'substract') {
+              operation.push(pad[key])
+              current.push(pad[key])
+              break
+            }
+          }
+
+          if (operation.length == 0) {
+            console.log('you might want to type a number first');
+            break
+          }
+
+          if (operators.includes(lastKey)) {
+            operation.pop(1)
+            operation.push(pad[key])
+            console.log('you need to pick just one operator');
+            current = []
+            break;
+          }
+
           operation.push(pad[key])
           current = []
           break;
@@ -62,8 +92,13 @@ document.addEventListener('click', function(e) {
           break;
 
         case 'equals':
+          if (operators.includes(lastKey)) {
+            operation.pop(1)
+            console.log('we\'ve ignored that last ' + lastKey);
+          }
+
           current = [eval(operation.join(''))]
-          operation = []
+          operation.push(pad[key])
           break
         default:
       }
